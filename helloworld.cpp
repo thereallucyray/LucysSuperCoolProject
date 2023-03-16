7f523bcc76755f2b835933867313ba17f5d44e99
thereallucyray <lucy@therayfamily.org>
2023-03-16 14:40:03 -0500

    Lucy added a hello world cpp program file

---
+++ helloworld.cpp
@@ -0,0 +1,15 @@
+//
+//  helloworld.cpp
+//  
+//
+//  Created by Lucy Ray on 3/16/23.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello world!" << endl;
+    
+    return 0;
+}
